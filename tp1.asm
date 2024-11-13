b95ef0f70fe42d1bcf606338312f382ae22185d8
Nicolas Cazal <nicolas.cazal02@gmail.com>
2024-11-13 01:18:30 +0000

    Arreglo un error con la etiqueta main

--- tp1.asm
+++ tp1.asm
@@ -3,15 +3,15 @@
 %include "constantes.asm"
 %include "variables.asm"
 
-global juego
+global main
 
 section     .text
-juego:
+main:
 ;Idea:
     ;mensaje_inicial
     ;establecer_configuracion (Aca se configuraria la partida ya sea de manera personalizada o predeterminada)
     ;procesar_input (Esta parte procesa el input que recibe del usuario y llama a las macros que corresponden (moverSoldado, guardarPartida, recuperarPartida, salirDelJuego, etc.))
-    ;verificar_partida (Si todavia no termino la partida, redirijo a la etiqueta 'juego', si no, redirije al fin del programa)
+    ;verificar_partida (Si todavia no termino la partida, redirijo a la etiqueta 'main', si no, redirije al fin del programa)
 ;fin_del_juego: (etiqueta de fin del juego)
     ;mostrar_estadisticas
     ;saludo_final
