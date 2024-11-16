33643743d838c5a8bfa14d9a8da96745d89b027a
Nicolas Cazal <nicolas.cazal02@gmail.com>
2024-11-15 22:37:06 -0300

    Añado variables de comando de movimiento

---
+++ movimiento.asm
@@ -0,0 +1,9 @@
+section     .data
+    movimiento_arriba db 'W'
+    movimiento_abajo db 'S'
+    movimiento_derecha db 'D'
+    movimiento_izquierda db 'A'
+    movimiento_arriba_derecha db 'E'
+    movimiento_arriba_izquierda db 'Q'
+    movimiento_abajo_derecha db 'C'
+    movimiento_abajo_izquierda db 'Z'
